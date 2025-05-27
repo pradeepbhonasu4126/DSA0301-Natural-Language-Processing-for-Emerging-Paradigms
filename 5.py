9fd8cdb20509bdfd6a2f2414a07895975bcc304b
Pradeep Bhonasu <107177802+pradeepbhonasu4126@users.noreply.github.com>
2025-05-27 14:16:25 +0530

    Add files via upload
    running -> run
    runs -> run
    runner -> runner
    easily -> easili
    fairly -> fairli
    cats -> cat
    studies -> studi

---
+++ 5.py
@@ -0,0 +1,68 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/student/Documents/dsa03/1.py ================
+Email found: example123@gmail.com
+Phone numbers found: ['987-654-3210']
+URL found: https://example.com
+Text after email redaction: My email is [EMAIL] and my phone number is 987-654-3210. Visit https://example.com
+Text starts with 'My'
+Tokens: ['My', 'email', 'is', 'example123@gmail', 'com', 'and', 'my', 'phone', 'number', 'is', '987-654-3210', 'Visit', 'https://example', 'com']
+
+================ RESTART: C:/Users/student/Documents/dsa03/2.py ================
+Input: 'ab' => Accept: True
+Input: 'aab' => Accept: True
+Input: 'cab' => Accept: True
+Input: 'baba' => Accept: False
+Input: 'abc' => Accept: False
+Input: 'abab' => Accept: True
+
+================ RESTART: C:/Users/student/Documents/dsa03/3.py ================
+[nltk_data] Downloading package wordnet to
+[nltk_data]     C:\Users\student\AppData\Roaming\nltk_data...
+[nltk_data]   Package wordnet is already up-to-date!
+[nltk_data] Downloading package omw-1.4 to
+[nltk_data]     C:\Users\student\AppData\Roaming\nltk_data...
+[nltk_data]   Package omw-1.4 is already up-to-date!
+Word	PorterStemmer	LancasterStemmer	Lemmatizer (verb)
+running	run		run			run
+runs	run		run			run
+ran	ran		ran			run
+easily	easili		easy			easily
+fairly	fairli		fair			fairly
+better	better		bet			better
+>>> 
+================ RESTART: C:/Users/student/Documents/dsa03/4.py ================
+cat -> cats
+dog -> dogs
+bus -> buses
+box -> boxes
+buzz -> buzzes
+church -> churches
+brush -> brushes
+city -> cities
+day -> days
+>>> 
+==== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python311/5.py ====
+[nltk_data] Downloading package punkt to
+[nltk_data]     C:\Users\student\AppData\Roaming\nltk_data...
+[nltk_data]   Package punkt is already up-to-date!
+running -> run
+runs -> run
+runner -> runner
+easily -> easili
+fairly -> fairli
+cats -> cat
+studies -> studi
+>>> 
+==== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python311/5.py ====
+[nltk_data] Downloading package punkt to
+[nltk_data]     C:\Users\student\AppData\Roaming\nltk_data...
+[nltk_data]   Package punkt is already up-to-date!
+running -> run
+runs -> run
+runner -> runner
+easily -> easili
+fairly -> fairli
+cats -> cat
+studies -> studi
